87be17792aaf3498365cfe4a112b4f3c3323ae02
akalenderov <akalenderov@UPD-99>
2023-07-10 18:55:44 +0600

    Добавьте файлы проекта.

---
+++ 2023-07-10 choosing of the colors/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter your fav color");
+        Console.WriteLine("Your can choose one of these: ");
+        Console.WriteLine("- blue");
+        Console.WriteLine("- green");
+        Console.WriteLine("- cyan");
+        var color = Console.ReadLine();
+
+        if (color == "blue")
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.Black;
+        }//end if_1
+        else if (color == "green")
+        {
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.Black;
+        }//end else_if_2
+        else
+        {
+            Console.BackgroundColor = ConsoleColor.Cyan;
+            Console.ForegroundColor = ConsoleColor.Black;
+        }//end else
+
+    }//end public
+}//end class
